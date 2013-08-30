a2e1e7fd50a7046718099f087a46c0be735a4d7b
laksjpoid <mengtao09@foxmail.com>
2013-08-30 22:33:55 +0800

    form parameter - lesson8

---
+++ springMVC/src/springmvc/web/annotation/DataController.java
@@ -0,0 +1,24 @@
+package springmvc.web.annotation;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/data")
+public class DataController {
+	
+	@RequestMapping("/addUser")
+	public String addUser(String userName, String age, HttpServletRequest request){
+		request.setAttribute("userName", userName);
+		request.setAttribute("age", age);
+		return "addUser";
+	}
+	
+	@RequestMapping("/delUser")
+	public String delUser(){
+		String result = "I will del user. - ”≈ªØ";
+		return "annotation1";
+	}
+}
